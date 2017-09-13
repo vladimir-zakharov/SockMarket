bd2de8f901fb93d15ddafd4ff24338127b114745
vladimir-zakharov <vladimir.zakharov93@gmail.com>
2017-09-13 13:04:29 +0300

    installed Entity Framework

---
+++ SockMarket/Models/Company.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SockMarket.Models
+{
+    public class Company
+    {
+        public int ID { get; set; }
+        public int Name { get; set; }
+    }
+}
