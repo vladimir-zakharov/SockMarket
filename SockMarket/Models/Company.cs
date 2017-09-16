c9f39dc192f702d07659e2537661e50b54c94858
vladimir-zakharov <vladimir.zakharov93@gmail.com>
2017-09-16 16:48:41 +0300

    index pages ui changes

--- SockMarket/Models/Company.cs
+++ SockMarket/Models/Company.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace SockMarket.Models
@@ -7,6 +8,7 @@ namespace SockMarket.Models
     {
         public int ID { get; set; }
         [Required]
+        [DisplayName("Company Name")]
         public string Name { get; set; }
 
         public virtual ICollection<Contact> Contacts { get; set; }
